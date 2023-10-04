332d184c9b0cd033f330ae122c1504798cde8342
Sid Hathi <sidhathi20@gmail.com>
2023-10-03 22:37:58 -0400

    code styling tweak

--- src/components/Setup/UserSetup.tsx
+++ src/components/Setup/UserSetup.tsx
@@ -3,13 +3,13 @@ import { View, Button, Heading, Input, Center, Text } from 'native-base';
 import useAppDispatch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
 import { authSelector, setUserInitialized } from 'redux/slices/authSlice';
-import { getUser, initUser, userDataSelector } from 'redux/slices/userDataSlice';
+import { initUser, userDataSelector } from 'redux/slices/userDataSlice';
 import { UserScopes } from 'types/users';
 
 export default function UserSetup() {
   const [name, setName] = useState<string | undefined>();
   const dispatch = useAppDispatch();
-  const { fbUserRef, authenticated } = useAppSelector(authSelector);
+  const { fbUserRef } = useAppSelector(authSelector);
   const { userData } = useAppSelector(userDataSelector);
 
   const handleSubmit = useCallback(async () => {
