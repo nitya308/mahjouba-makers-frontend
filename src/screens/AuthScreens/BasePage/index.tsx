6c83d7df30197320b3fbdc73fdddc161affe9900
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2023-10-18 12:03:56 -0400

    tts

--- src/screens/AuthScreens/BasePage/index.tsx
+++ src/screens/AuthScreens/BasePage/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Speech from 'expo-speech';
 import { useNavigation } from '@react-navigation/native';
 import { View } from 'react-native';
 import BaseView from 'components/BaseView';
@@ -10,9 +11,33 @@ import Colors from 'utils/Colors';
 const BasePage = () => {
   const navigation = useNavigation<NavType>();
 
+  const speak = () => {
+    const thingToSay = 'مرحبا، هذا اختبار';
+    Speech.speak(thingToSay, { language: 'ar' });
+  };
+
+  const speak2 = () => {
+    const thingToSay = 'Hello, this is a test';
+    Speech.speak(thingToSay, { language: 'en' });
+  };
+
   return (
     <BaseView logoText={'App Title'}>
       <View style={{ width: '90%' }}>
+        <AppButton
+          onPress={speak}
+          title={'Speak (Arabic)'}
+          fullWidth
+          backgroundColor={Colors.primary}
+          textColor='white'
+        />
+        <AppButton
+          onPress={speak2}
+          title={'Speak (English)'}
+          fullWidth
+          backgroundColor={Colors.primary}
+          textColor='white'
+        />
         <AppButton
           onPress={() => navigation.navigate(AuthStackRoutes.SIGNIN)}
           title={'Log In'}
