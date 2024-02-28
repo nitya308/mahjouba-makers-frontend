5eda88c183a38f35f1f8c6e880ed422dee156d78
SoniaBendre <soniabendre02@gmail.com>
2024-02-28 17:13:27 -0500

    test

--- src/controllers/AuthController/Welcome.tsx
+++ src/controllers/AuthController/Welcome.tsx
@@ -1,17 +1,24 @@
 import React, { useCallback, useRef, useState, useEffect } from 'react';
-import { Text, Input, VStack, Center, Button, Box, ScrollView } from 'native-base';
+import { Text, Input, VStack, Center, Button, Box, ScrollView, IconButton } from 'native-base';
 import SharpButton from 'components/SharpButton';
 import i18next from 'i18next';
 import { Languages } from 'types/user';
 import { SafeAreaView } from 'react-native';
-
+import * as Speech from 'expo-speech';
+import AudioIcon from '../../assets/audio_icon.svg';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Highlighter } from 'react-highlighter';
 
 export default function Welcome({ navigation }) {
+
+  const textToHighlight = 'Welcome to the app. This is a test of the audio feature.';
 
   const handleLanguageSelection = (language: Languages) => {
     i18next.changeLanguage(language);
     navigation.navigate('Authenticate');
   };
+
+  const searchWords = ['quick', 'brown', 'lazy'];
 
   return (
     <SafeAreaView>
@@ -20,6 +27,18 @@ export default function Welcome({ navigation }) {
           <SharpButton w='100%' my='10px'
             size='sm' onPress={() => handleLanguageSelection(Languages.EN)}>
             <Text color='black' fontWeight='medium'>Welcome</Text>
+            <Highlighter
+              highlightClassName="YourHighlightClass"
+              searchWords={searchWords}
+              autoEscape={true}
+              textToHighlight={textToHighlight}
+            />
+            <IconButton
+              icon={<AudioIcon />}
+              onPress={() => {
+                Speech.speak((text), { language: i18next.language });
+              }}
+            />
           </SharpButton>
           <SharpButton w='100%' my='10px'
             size='sm' onPress={() => handleLanguageSelection(Languages.FR)}>
