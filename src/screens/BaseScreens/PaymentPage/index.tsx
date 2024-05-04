13f25001449288482ebd540ba043cf1a83b04cfd
nitya-cs <nitya.agarwala@gmail.com>
2024-05-04 18:07:30 -0400

    basic template for new page in app

--- src/screens/BaseScreens/PaymentPage/index.tsx
+++ src/screens/BaseScreens/PaymentPage/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import TextHighlighter from 'components/SpeechHighlighter';
+import { IconButton } from 'native-base';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
+import AppStyles from 'styles/commonstyles';
+import AudioIcon from '../../../assets/audio_icon.svg';
 
 const PaymentPage = () => {
+
+  const { t } = useTranslation();
+  const [pressed, setPressed] = useState(false);
+
   return (
-    <View>
-      <Text>PaymentPage</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView style={AppStyles.mainContainer}>
+        <TextHighlighter style={AppStyles.left_heading} text={t('Earnings')} pressed={pressed} setPressed={setPressed} />
+      </ScrollView>
+      <IconButton
+        icon={<AudioIcon />}
+        onPress={() => {
+          setPressed(true);
+        }}
+        style={AppStyles.audioButtonStyle}
+      />
+    </SafeAreaView>
   );
 };
 
